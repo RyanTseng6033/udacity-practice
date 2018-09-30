d77455ec85ef35f219c7308da799c79fb6f20943
RyanTseng6033 <43104026+RyanTseng6033@users.noreply.github.com>
2018-09-30 10:43:26 +0800

    Add files via upload
    還有一個問題。

--- days.py
+++ days.py
@@ -12,10 +12,12 @@ november = 30
 december = 31
 time = input("請輸入第一個時間:")
 time = time.split("/")
-year = (int(time[0])-1)
-year1 = int(year) / 4
-year1 = int(year1)
-year = int(year) * 365 + int(year1)
+day = time[2]
+year = (int(time[0]))
+if (year% 4== 0 and year% 100!= 0) or (year% 400== 0):
+    day += 1
+year = (int(time[0])-1)    
+year = int(year) * 365
 month = (int(time[1])-1)
 if str(month) == "1":
     month = int(january)
@@ -41,14 +43,15 @@ elif str(month) == "11":
     month = int(january+february+march+april+may+june+july+august+september+october+november)
 elif str(month) == "12":
     month = int(january+february+march+april+may+june+july+august+september+october+november+december)    
-day = time[2]
 totle1 = int(year) + int(month) + int(day)
 time2 = input("請輸入第二個時間:")
 time2 = time2.split("/")
+day2 = int(time2[2])
+year2 = (int(time2[0]))
+if (year2% 4== 0 and year2% 100!= 0) or (year2% 400== 0):
+    day2 += 1
 year2 = (int(time2[0])-1)
-year3 = int(year2) / 4
-year3 = int(year3)
-year2 = int(year2) * 365 + int(year3)
+year2 = int(year2) * 365
 month2 = (int(time2[1])-1)
 if str(month2) == "1":
     month2 = int(january)
@@ -74,7 +77,6 @@ elif str(month2) == "11":
     month2 = int(january+february+march+april+may+june+july+august+september+october+november)
 elif str(month2) == "12":
     month2 = int(january+february+march+april+may+june+july+august+september+october+november+december)    
-day2 = time2[2]
 totle2 = int(year2) + int(month2) + int(day2)
-totle = (int(totle2) - int(totle1))
+totle = (int(totle2) - int(totle1) + 1)
 print("兩個時間中間相差" + str(totle) + "天")
